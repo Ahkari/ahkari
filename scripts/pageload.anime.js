7682b7ab724ba974c4923b7b500332b0e2ff7097
Ahkari <gqz825584053@qq.com>
2015-03-16 13:18:06 +0800

    场景二雨滴动画完成。
    具体参数可自行调节。

--- scripts/pageload.anime.js
+++ scripts/pageload.anime.js
@@ -17,13 +17,13 @@ var storyLoadEvent=function(){
 	// 第一个场景，ajax请求页面，向其中添加雨滴，然后绑摄像机插件动画
 	function senceOne(){
 		$(".storyBack").css("transform","scale(1.1)");
-		// 下面需要ajax请求到story_sence_one的html，然后生成三十个水滴。
+		// 下面需要ajax请求到story_sence_one的html，然后生成水滴。
 		$.ajax("./htm/story_sence_one.htm",{
 				async:true,
 				// complete:complete,
 				dataType:"html",
 				error:null,
-				success:story.addRainDom,//无需addRainDom指定后面三个参数
+				success:story.addRainDom,//addRainDom无需指定后面三个参数
 				type:"get",
 			}
 		);
@@ -60,19 +60,19 @@ var story={
 		$(".storyBack").html(data);
 		var rainDom;
 		for (var i=0;i<70;i++){
-			var thisWidth=getRandom(20)+10;
+			var thisWidth=getRandom(20)+10;//设定雨滴粒子大小在10-30之间
 			rainDom+="<div id=\"obj"+i+"\" style=\"width:30px;height:30px\"><img src=\"./src/rain.png\" width=\""+thisWidth+"\" height=\""+thisWidth+"\" /></div>";
 	
 		}
 		$("#sence_one").html(rainDom);
-		// 下面为生成的桑拿室水滴绑动画
+		// 下面为生成的水滴绑动画
 		story.bindCameraAnime();
 		setTimeout(story.whiteLayerTransform,2500);
 
 	},
 	// 给元素绑定动画，此处是摄像机插件。
 	bindCameraAnime:function(){
-		// story下的一个数组，为摄像机下雨滴位置数组。想办法自己生成。
+		// 雨滴数组。
 		var obejectArray=[];
 		for(var i=0;i<70;i++){
 			var eachObj={
@@ -87,8 +87,7 @@ var story={
 			eachObj.z_position=getRandom(600);
 			obejectArray.push(eachObj);
 		};
-		// console.log(obejectArray);
-		// alert(obejectArray);
+		
 		// ！！！！！使用俺自己做的插件来实现摄相机效果！！！！！
 		$("#sence_one").camera3D(obejectArray,{
 			mode: "once",
@@ -117,16 +116,125 @@ var story={
 		// $(".storyScenceTwo").css("display","block");
 
 		function insideWhiteLayerTransform(){
-			$(".storyScenceTwo").css("display","block");
+			$(".storyScenceTwo").css("display","block");	//场景二出现
+			story.createRainDrop(100);	//制造下雨，设为100个雨滴。
 			
-			
-			$(".whiteTransformLayer").css("opacity",0);
+			$(".whiteTransformLayer").css("opacity",0);		//白场过渡
 		}
-		// alert(1);
-		// console.log(1);
+
 
 
 	},	//whiteLayerTransform:function()
+
+	//制造下雨，包括初始场景和单粒子增加的两个过程。
+	createRainDrop:function(num){
+		var rainDropDom;
+		var rainDropDomCopy;
+		var thisPathArr=[];
+		var thisPathArrCopy=[];
+		var thisTimeArr=[];
+		var thisTimeArrCopy=[];
+
+		for (var i=0;i<40;i++){
+			var thisWidth=getRandom(15)+10;//设定雨滴粒子大小在10-25之间
+			var thisTop=getRandom(1000)-500;	//起始高度,注意运算规则.考虑正负。
+			var thisPath=thisTop+2000;	//路径，由起始高度而来
+			thisPathArr.push(thisPath);	
+			var thisTime=Math.floor(250000/thisWidth);	//运行时长，由粒子大小而来，应为遵照近大远小，近快远慢原则。
+			thisTimeArr.push(thisTime);
+			// var thisTop=getRandom(1000);
+			var thisLeft=getRandom(2000)-200;	//起始x位置，考虑正负。
+			rainDropDom+="<div id=\"rainDropDom"+i+"\" style=\"width:30px;height:30px;position:absolute;top:"+thisTop+"px;left:"+thisLeft+"px\"><img src=\"./src/rain.png\" width=\""+thisWidth+"\" height=\""+thisWidth+"\" /></div>";
+		
+		}
+		$(".stroyBackTwoOver").html(rainDropDom);
+		$(".stroyBackTwoOver div").each(function(index){
+			$(this).animate({top:"+="+thisPathArr[index]+"px"},thisTimeArr[index],"linear");
+		});	//$(".stroyBackTwoOver div").each(function(index)
+
+		//第二层
+		for (var i=0;i<50;i++){
+			var thisWidthCopy=getRandom(15)+5;//设定雨滴粒子大小在10-25之间
+			var thisTopCopy=getRandom(1000)-500;	//起始高度,注意运算规则.考虑正负。
+			var thisPathCopy=thisTopCopy+2000;	//路径，由起始高度而来
+			thisPathArrCopy.push(thisPathCopy);	
+			var thisTimeCopy=Math.floor(250000/thisWidthCopy);	//运行时长，由粒子大小而来，应为遵照近大远小，近快远慢原则。
+			thisTimeArrCopy.push(thisTimeCopy);
+			// var thisTop=getRandom(1000);
+			var thisLeftCopy=getRandom(2000)-200;	//起始x位置，考虑正负。
+			rainDropDomCopy+="<div id=\"rainDropDomCopy"+i+"\" style=\"width:30px;height:30px;position:absolute;top:"+thisTopCopy+"px;left:"+thisLeftCopy+"px\"><img src=\"./src/rain.png\" width=\""+thisWidthCopy+"\" height=\""+thisWidthCopy+"\" /></div>";
+			// console.log(rainDropDomCopy);
+		}
+		$(".storyBackTwo").html(rainDropDomCopy);
+		$(".storyBackTwo div").each(function(indexCopy){
+			$(this).animate({top:"+="+thisPathArrCopy[indexCopy]+"px"},thisTimeArrCopy[indexCopy],"linear");
+			console.log($(this));
+		});	//$(".stroyBackTwoOver div").each(function(index)
+
+
+		//开始粒子生成。
+		
+		
+		var rainDomCreateAnime=setInterval(story.createOneRainDrop,200);
+		// var testThisAnime=setInterval(story.testInterval,500);
+	},	//createRainDrop:function()
+
+	
+	//单粒子生成
+	// createOneRainDrop:function(minWidth,offsetWidth,minTop,offsetTop,minLeft,offsetLeft){
+	createOneRainDrop:function(minWidth,offsetWidth,minTop,offsetTop,minLeft,offsetLeft){
+		// var thisWidth=getRandom(offsetWidth)+minWidth;
+		// var thisTop=getRandom(offsetTop)+minTop;
+		// var thisPath=thisTop+2000;
+		// var thisTime=Math.floor(250000/thisWidth);
+		// var thisLeft=getRandom(offsetLeft)+minLeft;
+
+		var thisWidth=getRandom(15)+10;//设定雨滴粒子大小在10-25之间
+		var thisTop=getRandom(500)-500;	//起始高度,注意运算规则.考虑正负。
+		var thisPath=thisTop+2000;	//路径，由起始高度而来
+		var thisTime=Math.floor(250000/thisWidth);	//运行时长，由粒子大小而来，应为遵照近大远小，近快远慢原则。
+		var thisLeft=getRandom(2000)-200;
+
+		//粒子计数
+		// console.log($("#rainDomCount").val());
+		var index=Number($("#rainDomCount").val())+1;
+		$("#rainDomCount").val(index);
+		
+		var rainDropOneDom="<div id=\"rainDropOneDom"+index+"\" style=\"width:30px;height:30px;position:absolute;top:"+thisTop+"px;left:"+thisLeft+"px\"><img src=\"./src/rain.png\" width=\""+thisWidth+"\" height=\""+thisWidth+"\" /></div>";
+		$(".stroyBackTwoOver").append(rainDropOneDom);
+		$("#rainDropOneDom"+index).animate({top:"+="+thisPath+"px"},thisTime,"linear");
+
+
+		var thisWidthCopy=getRandom(15)+5;//设定雨滴粒子大小在10-25之间
+		var thisTopCopy=getRandom(500)-500;	//起始高度,注意运算规则.考虑正负。
+		var thisPathCopy=thisTopCopy+2000;	//路径，由起始高度而来
+		var thisTimeCopy=Math.floor(250000/thisWidthCopy);	//运行时长，由粒子大小而来，应为遵照近大远小，近快远慢原则。
+		var thisLeftCopy=getRandom(2000)-200;
+
+		//粒子计数
+		// console.log($("#rainDomCount").val());
+		var indexCopy=Number($("#rainDomCountCopy").val())+1;
+		$("#rainDomCountCopy").val(indexCopy);
+		
+		var rainDropOneDomCopy="<div id=\"rainDropOneDomCopy"+indexCopy+"\" style=\"width:30px;height:30px;position:absolute;top:"+thisTopCopy+"px;left:"+thisLeftCopy+"px\"><img src=\"./src/rain.png\" width=\""+thisWidthCopy+"\" height=\""+thisWidthCopy
+		+"\" /></div>";
+		
+		$(".storyBackTwo").append(rainDropOneDomCopy);
+		$("#rainDropOneDomCopy"+indexCopy).animate({top:"+="+thisPathCopy+"px"},thisTimeCopy,"linear");
+		// console.log(index);
+		index++;
+		indexCopy++;
+	},	//createOneRainDrop:function()
+	testInterval:function(){
+		
+		console.log($("#rainDomCount").val());//粒子计数。绑定在页面的隐藏input上。
+		var test=Number($("#rainDomCount").val())+1;
+		$("#rainDomCount").val(test);
+		
+	
+	
+		
+	}
 
 }	//story命名空间
 
