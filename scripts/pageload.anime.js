c026a3ebc1a4f1c6032efdb411b8c5ad7e5ca0cc
Ahkari <gqz825584053@qq.com>
2015-03-18 14:15:02 +0800

    beautifulPage实现碎片视差动画。
    将碎片视差动画插件化。

--- scripts/pageload.anime.js
+++ scripts/pageload.anime.js
@@ -9,6 +9,7 @@ var countingWaveLoadEvent=function(){
 var triggerFingerLoadEvent=function(){
 	console.log(pageIndexName);
 	$(".storyBack").css("transform","scale(1.8)");
+	window.clearInterval(rainDomCreateAnime);
 
 };
 
@@ -45,6 +46,9 @@ var storyLoadEvent=function(){
 var beautifulWorldLoadEvent=function(){
 	console.log(pageIndexName);
 	$(".storyBack").css("transform","scale(1.8)");
+	window.clearInterval(rainDomCreateAnime);
+
+	beautiful.cutCard($("#beautifulTitle"));
 
 };
 var attackOnTitanLoadEvent=function(){
@@ -255,8 +259,73 @@ var story={
 
 }	//story命名空间
 
+// beautifulWorld命名空间，内含beautifulWorld页面的专有操作函数。
+var beautiful={
+	//元素卡片化。
+	//特例模式，即对指定的517*56像素图片分割。X分12部分，y随机分2,3块。
+	cutCard:function(cardDom){
+		var cardHeight=cardDom.height();
+		var cardWidth=cardDom.width();
+
+		var cardDomName=cardDom.attr("id");
+
+		var cardCopyObjects;
+
+		var unitX=cardWidth/12;
+		var unitY1=cardHeight/2;
+		var unitY2=cardHeight/3;
+
+		var creatTitleCopy="";
+		var eachTitle;
+
+		for (var i=0;i<12;i++){
+			var isRandom=(i%2==0)?2:3;
+			unitY=(i%2==0)?unitY1:unitY2;
+			for (var j=0;j<isRandom;j++){
+				var left=i*unitX;
+				var top=j*unitY;
+				var width=unitX;
+				var height=unitY;
+
+				var styleStr="style=\"position:absolute;z-index:10;left:"+left+"px;top:"+top+"px;width:"+width+"px;height:"+height+"px;background:url('./src/beautifulWorldTitle.png') no-repeat -"+left+"px -"+top+"px;\"";
+				
+				eachTitle="<div id=\""+cardDomName+"_cardCopy"+i+"_"+j+"\" "+styleStr+"></div>";
+				creatTitleCopy+=eachTitle;
+	
+			}	//for (var j=0;j<isRandom;j++)
 
 
+		}	//for (var i=0;i<12;i++)
+		
+	
+		cardDom.html(creatTitleCopy);
+
+		// 绑动画
+
+		for (var i=0;i<12;i++){
+			var isRandom=(i%2==0)?2:3;
+			for (var j=0;j<isRandom;j++){
+				var randomY=getRandom(300)+500;
+
+				var title_y_path="+="+randomY.toString()+"px";
+				var title_y_path_ready="-="+randomY.toString()+"px";
+				var time=getRandom(300)+1000;
+				$("#"+cardDomName+"_cardCopy"+i+"_"+j)
+				.animate({
+					top:(title_y_path_ready)
+				},0)
+				.animate({
+					top:title_y_path
+				},time,"swing",function(){});
+
+			}
+		}
+
+
+		
+	},//cutCard:function(cardDom)
+
+}
 
 
 
